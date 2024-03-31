6ba1e3b06c468b0fde067f9b1f2ac76df534322c
DP1117 <dpark070111@gmail.com>
2024-03-31 18:14:56 -0400

    css animations

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ function myFunction() {
     x.className = "nav";
   }
 }
+
+// animation
+const textWrappers = document.querySelectorAll('.ml3');
+textWrappers.forEach((textWrapper) => {
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  anime.timeline({loop: false})
+    .add({
+      targets: '.letter',
+      opacity: [0,1],
+      easing: "easeInOutQuad",
+      duration: 1500,
+      delay: (el, i) => 10 * i
+    }).add({
+      targets: '.letter',
+      duration: 10,
+      easing: "easeOutExpo",
+      delay: 10
+    });
+});
